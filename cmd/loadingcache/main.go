fbbb58659d11154cccc79e2dcd286142b0bd313f
huangjinbing <bingoo.huang@gmail.com>
2024-03-26 20:23:48 +0800

    loadingcache cli

--- cmd/loadingcache/main.go
+++ cmd/loadingcache/main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"github.com/goldstd/loadingcache"
+
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/goldstd/loadingcache"
+	"github.com/pkg/errors"
 	"github.com/xo/dburl"
 )
 
 var (
-	expire = flag.Duration("expire", 15*time.Second, "expiration")
-	query  = flag.String("query", "select v from gcache where k = ?", "query SQL")
-	dsn    = flag.String("dsn", "mysql://root:root@tcp(127.0.0.1:3306)/gcache", "DSN")
-	port   = flag.Int("port", 8080, "Port to listen on")
+	expire     = flag.Duration("expire", 5*time.Second, "expiration")
+	query      = flag.String("query", "select v from gcache where k = ?", "query SQL")
+	dsn        = flag.String("dsn", "mysql://root:root@127.0.0.1:3306/gcache", "DSN")
+	port       = flag.Int("port", 8080, "Port to listen on")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 )
 
 var cache loadingcache.Cache
@@ -31,8 +41,9 @@ func main() {
 		log.Printf("Error parsing DSN: %v", err)
 		return
 	}
-
 	defer db.Close()
+
+	defer profiling()()
 
 	dbLoader := &DBLoader{DB: db, Query: *query}
 
@@ -43,8 +54,14 @@ func main() {
 	}.Build()
 
 	http.HandleFunc("/", httpHandle)
-	if err := http.ListenAndServe(":"+strconv.Itoa(*port), nil); err != nil {
-		log.Printf("Error listening on port %d: %v", *port, err)
+	server := &http.Server{
+		Addr: ":" + strconv.Itoa(*port),
+	}
+
+	go signalCapture(server)
+
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("HTTP server error: %v", err)
 	}
 
 	log.Printf("exiting")
@@ -59,9 +76,11 @@ func httpHandle(w http.ResponseWriter, r *http.Request) {
 	value, err := cache.Get(key)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	} else {
+	} else if str, ok := value.(string); ok {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		_, _ = w.Write([]byte(value.(string)))
+		_, _ = w.Write([]byte(strconv.Quote(str)))
+	} else {
+		log.Printf("invalid val: %+v", value)
 	}
 }
 
@@ -71,8 +90,71 @@ type DBLoader struct {
 }
 
 func (d *DBLoader) Load(key any) (any, error) {
-	row := d.DB.QueryRow(d.Query, key)
-	var value string
-	err := row.Scan(&value)
-	return value, err
+	row, err := d.DB.Query(d.Query, key)
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+
+	var val sql.NullString
+
+	if row.Next() {
+		if err := row.Scan(&val); err != nil {
+			return nil, err
+		}
+	}
+
+	return val.String, nil
+}
+
+func signalCapture(closeables ...io.Closer) {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	for _, cl := range closeables {
+		if cl != nil {
+			if err := cl.Close(); err != nil {
+				log.Printf("close %t: %v", cl, err)
+			}
+		}
+	}
+}
+
+func profiling() func() {
+	var deferFuncs []func()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		deferFuncs = append(deferFuncs, func() {
+			_ = f.Close()
+		})
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+
+		deferFuncs = append(deferFuncs, pprof.StopCPUProfile)
+	}
+
+	if *memprofile != "" {
+		deferFuncs = append(deferFuncs, func() {
+			f, err := os.Create(*memprofile)
+			if err != nil {
+				log.Fatal("could not create memory profile: ", err)
+			}
+			defer f.Close()
+			runtime.GC() // get up-to-date statistics
+			if err := pprof.WriteHeapProfile(f); err != nil {
+				log.Fatal("could not write memory profile: ", err)
+			}
+		})
+	}
+
+	return func() {
+		for i := len(deferFuncs) - 1; i >= 0; i-- {
+			deferFuncs[i]()
+		}
+	}
 }
